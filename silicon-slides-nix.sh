4bcded2a09af83d096bd37a0d7e7b4f82d07ff52
aabccd021 <aabccd021@gmail.com>
2025-02-18 11:30:09 +0700

    Add temporary directory for slide normalization

--- silicon-slides-nix.sh
+++ silicon-slides-nix.sh
@@ -10,12 +10,14 @@ export XDG_CONFIG_HOME="$PWD/config"
 export SILICON_CACHE_PATH="$PWD/silicone-cache"
 
 converted_slides=$(mktemp)
+tmpdir=$(mktemp -d)
 while IFS= read -r line; do
   filename=$(echo "$line" | awk '{print $1}')
   args=$(echo "$line" | awk '{$1=""; print $0}')
 
   # remove nix store hash
   normalized=$(echo "$filename" | xargs basename | sed 's/^[^-]*-//')
+  normalized="$tmpdir/$normalized"
 
   cp -Lv "$filename" "$normalized"
   echo "$normalized $args" >>"$converted_slides"
