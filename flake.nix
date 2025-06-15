c286059548b9712ac5a5f2040e322eb0e8069ea6
aabccd021 <aabccd021@gmail.com>
2025-06-16 06:53:03 +0700

    Add flake.nix: disallow import, collect and link inputs

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+  nixConfig.allow-import-from-derivation = false;
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
@@ -6,6 +7,23 @@
 
   outputs = { self, ... }@inputs:
     let
+
+      lib = inputs.nixpkgs.lib;
+
+      collectInputs =
+        is:
+        pkgs.linkFarm "inputs" (
+          builtins.mapAttrs
+            (
+              name: i:
+              pkgs.linkFarm name {
+                self = i.outPath;
+                deps = collectInputs (lib.attrByPath [ "inputs" ] { } i);
+              }
+            )
+            is
+        );
+
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
 
       silicon-slides = pkgs.writeShellApplication {
@@ -65,6 +83,7 @@
         snapshot-test = snapshot-test;
         formatting = treefmtEval.config.build.check self;
         formatter = treefmtEval.config.build.wrapper;
+        allInputs = collectInputs inputs;
       };
 
       gcroot = packages // {
